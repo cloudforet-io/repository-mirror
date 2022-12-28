0ec5b11cfb663f0d0e54f67e266b0bd7d346b06c
seolmin <seolmin@megazone.com>
2022-12-28 16:08:27 +0900

    feat: add create_resources_to_local_repository method in RepositoryResources
    Signed-off-by: seolmin <seolmin@megazone.com>

--- conf.py
+++ conf.py
@@ -5,3 +5,17 @@ API_KEY = '<API_TOKEN>'
 ENDPOINT = {
     'REPOSITORY': 'grpc+ssl://xxxxx.dev.spaceone.dev:443/v1',
 }
+
+INSTALLED_PLUGINS = [
+    # 'plugin-aws-cloud-service-inven-collector',
+    # 'plugin-aws-ec2-inven-collector',
+    # 'plugin-aws-phd-inven-collector',
+    # 'plugin-aws-trusted-advisor-inven-collector',
+    # 'plugin-azure-inven-collector',
+    # 'plugin-google-cloud-inven-collector',
+    # 'plugin-aws-cloudtrail-mon-datasource',
+    # 'plugin-aws-cloudwatch-mon-datasource',
+    # 'plugin-azure-activity-log-mon-datasource',
+    # 'plugin-azure-monitor-mon-datasource',
+    # 'plugin-google-stackdriver-mon-datasource'
+]
